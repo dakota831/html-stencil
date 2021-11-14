77a13dc893ae23effcca8e884cf2c278588ee2cb
dakota831 <drtorres831@gmail.com>
2021-11-14 11:48:43 -0500

    redundencies

--- 1.1 Basic Modal/webComponents.js
+++
@@ -1,9 +0,0 @@
-//Web component parts
-//Custom HTML Elements
-//Shadow DOM
-//Templates and Slots
-//HTML Imports *NOT IMPLEMENTED*
-
-//Allows us to encapsulate logic + UI into a single component
-//Allows us to reuse code
-
